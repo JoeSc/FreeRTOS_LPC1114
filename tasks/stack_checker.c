95de82c7b5c346849ae00cd60a6a947788430149
Joe Schaack <joe.schaack@gmail.com>
2011-02-19 22:28:47 -0600

    Implemented vPortUsedMem in the stack_checker task

--- tasks/stack_checker.c
+++ tasks/stack_checker.c
@@ -10,10 +10,17 @@ void stack_checker(void *pvParameters)
 
     while(1)
     {
+        int bytesFree;
+        int bytesUsed;
+        int blocksFree;
+        vPortUsedMem (&bytesFree, &bytesUsed, &blocksFree);
+        
         signed char list[200];
         vTaskList(list);
-        puts("\nName\t     State  Priority\tStack\tNum");
-        printf("%s",list);
+        
+        printf ("Task\t\tState\tPrty\tStack\tTask#\n");
+        printf ("%s\nHeap size=%d, used=%d, ", list, configTOTAL_HEAP_SIZE, bytesUsed);
+        printf("free=%d (%d blocks)\n", bytesFree, blocksFree);
         vTaskDelay(5000 / portTICK_RATE_MS);        
     }    
 }
